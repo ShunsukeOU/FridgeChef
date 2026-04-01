9f2f7e293ebc903c1a1ba0bc75236896bf9616c4
Shunsuke <kojinyoumail4service@icloud.com>
2026-04-01 19:25:50 +0900

    Initial Commit

---
+++ FridgeChef/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FridgeChef
+//
+//  Created by 平良隼涼 on 2026/04/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FridgeChef/FridgeChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeChefApp.swift
+//  FridgeChef
+//
+//  Created by 平良隼涼 on 2026/04/01.
+//
+
+import SwiftUI
+
+@main
+struct FridgeChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
